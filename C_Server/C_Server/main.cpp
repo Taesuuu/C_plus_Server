73bb7a1d9cd311301b4d09765541ef317ab93512
Taesuuu <taesu08@gmail.com>
2022-03-03 21:54:30 +0900

    Test Mac

---
+++ C_Server/C_Server/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C_Server
+//
+//  Created by 정태수 on 2022/03/03.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
